4e3090f1e63cb70b318f9429e432f15b0fe89df2
Nate <npzaldivar@gmail.com>
2025-01-01 18:24:30 -0700

    First Commit

---
+++ scenes/objects/chicken/walk_state.gd
@@ -0,0 +1,22 @@
+extends NodeState
+
+@export var character: CharacterBody2D
+@export var animated_sprite_2d: AnimatedSprite2D
+
+func _on_process(_delta : float) -> void:
+	pass # Called every frame while this state is active.
+
+
+func _on_physics_process(_delta : float) -> void:
+	pass # Called during physics updates.
+
+
+func _on_next_transitions() -> void:
+	pass
+
+
+func _on_enter() -> void:
+	animated_sprite_2d.play("walk")
+
+func _on_exit() -> void:
+	animated_sprite_2d.stop()
