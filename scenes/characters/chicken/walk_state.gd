bbf538411cf73b4768aaa369d308cda98ab89e09
Nate <npzaldivar@gmail.com>
2025-01-02 08:57:36 -0700

    Adding walk cycles

--- scenes/characters/chicken/walk_state.gd
+++ scenes/characters/chicken/walk_state.gd
@@ -1,6 +1,6 @@
 extends NodeState
 
-@export var character: CharacterBody2D
+@export var character: NonPlayableCharacter
 @export var animated_sprite_2d: AnimatedSprite2D
 @export var navigation_agent_2d: NavigationAgent2D
 @export var min_speed: float = 5.0
@@ -31,6 +31,7 @@ func _on_process(_delta : float) -> void:
 
 func _on_physics_process(_delta : float) -> void:
 	if navigation_agent_2d.is_navigation_finished():
+		character.current_walk_cycle += 1
 		set_movement_target()
 		return
 	
@@ -41,24 +42,27 @@ func _on_physics_process(_delta : float) -> void:
 	var velocity: Vector2 = target_direction * speed
 	
 	if navigation_agent_2d.avoidance_enabled:
+		animated_sprite_2d.flip_h = velocity.x < 0
 		navigation_agent_2d.velocity = velocity
 	else:
 		character.velocity = velocity
 		character.move_and_slide()
 	
 func on_safe_velocity_computed(safe_velocity: Vector2) -> void:
+	animated_sprite_2d.flip_h = safe_velocity.x < 0
 	character.velocity = safe_velocity
 	character.move_and_slide()
 
 
 func _on_next_transitions() -> void:
-	if navigation_agent_2d.is_navigation_finished():
+	if character.current_walk_cycle == character.walk_cycles:
 		character.velocity = Vector2.ZERO
 		transition.emit("Idle")
 
 
 func _on_enter() -> void:
 	animated_sprite_2d.play("walk")
+	character.current_walk_cycle = 0
 
 func _on_exit() -> void:
 	animated_sprite_2d.stop()
